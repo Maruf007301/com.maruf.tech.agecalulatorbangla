03e9ce2dfc4cf0d73053553a25693abc766826bb
maruf <rokeya1057@gmail.com>
2023-08-11 13:45:54 +0600

    motamuti final

--- app/src/main/java/com/ruetgmail/taufiqur/tipsntricks/data/preference/PrefKey.java
+++
@@ -1,5 +0,0 @@
-package com.ruetgmail.taufiqur.tipsntricks.data.preference;
-
-public class PrefKey {
-    public static final String APP_PREF_NAME = "tips_app";
-}
--- app/src/main/java/com/ruetgmail/taufiqur/tipsntricks/listeners/ListItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.ruetgmail.taufiqur.tipsntricks.listeners;
-
-import android.view.View;
-
-public interface ListItemClickListener {
-    public void onItemClick(int position, View view);
-}
